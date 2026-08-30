ffa84cfbb340cb1d3fdefb68a3d6ed76b8a172ac
yssssilva-bit <yssssilva@gmail.com>
2026-08-30 11:29:22 -0300

    Fix indentation and add print statement for sum

--- teste/src/main/java/Teste.java
+++ teste/src/main/java/Teste.java
@@ -2,6 +2,7 @@ package teste;
 public class Teste {
 public static void main(String[] args) {
 int num1 = 3, num2 = 5, soma;
-soma = num1 + num2;
+soma = num1 + num2; 
+  System.out.println(soma);
 }
 }
